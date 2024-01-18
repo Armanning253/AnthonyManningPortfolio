030a74f6e21e76ddd5419b5373e5b127c7164932
Tony Manning <armanning253@gmail.com>
2024-01-18 14:13:57 -0800

    remove args github and live from function and bracket commented out sections

--- components/project.tsx
+++ components/project.tsx
@@ -7,8 +7,9 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 type ProjectProps = (typeof projectsData)[number];
 
+// ! links commented out and deleted from function args
+
 export default function Project({ title, description,
-    /* gitHubLink, liveLink, */ // ! comment out links 
     tags, imageUrl }: ProjectProps) {
     const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
@@ -35,16 +36,20 @@ export default function Project({ title, description,
                     </p>
                     <div className='flex justify-center gap-2 my-6'>
                         <button
-                            className='bg-gray-900 text-white px-9 py-2 items-center rounded-full outline-none focus:scale-105 hover:scale-105 transition'>
-                            {/* <a href={gitHubLink} target='_blank' >
-                                GitHub
-                            </a> */} // ! comment out link
+                            className='bg-gray-900 text-white px-9 py-2 items-center rounded-full outline-none focus:scale-105 hover:scale-105 transition'
+                        >
+                            {/* <a href={gitHubLink} target='_blank' 
+                            </a> */}
+                            GitHub
+
                         </button>
                         <button
-                            className='bg-white dark:bg-gray-100 border-2  text-black/70 px-9 py-2  rounded-full focus:scale-105 hover:scale-105 transition'>
+                            className='bg-white dark:bg-gray-100 border-2  text-black/70 px-9 py-2  rounded-full focus:scale-105 hover:scale-105 transition'
+                        >
                             {/* <a href={liveLink} target='_blank' >
-                                Live
-                            </a> */} // ! comment out link
+                            </a> */}
+                            Live
+
                         </button>
                     </div>
 
